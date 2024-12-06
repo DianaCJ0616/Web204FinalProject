68914b96712f68a7bbaef705a804c52bb3fd0a80
Diana Cardenas-Jara <dianitacj1@gmail.com>
2024-12-05 21:16:10 -0600

    Added footer and header to each page

---
+++ AngelsCleaning.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const myLinks = document.getElementById("myLinks");
+    const logo = document.querySelector(".logo");
+
+    if (myLinks.style.display === "block") {
+        myLinks.style.display = "none";
+        logo.style.display = "block"; 
+    } else {
+        myLinks.style.display = "block";
+        logo.style.display = "none";
+    }
+}
--- AngelsLawn.js
+++ AngelsLawn.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     const myLinks = document.getElementById("myLinks");
-    const logo = document.querySelector(".logo"); // Select the logo element
+    const logo = document.querySelector(".logo"); 
 
     if (myLinks.style.display === "block") {
         myLinks.style.display = "none";
